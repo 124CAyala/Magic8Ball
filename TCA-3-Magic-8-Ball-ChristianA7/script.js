e399a58d5007b7ae030bbbbe0462059d0c61d5c9
124CAyala <90624249+124CAyala@users.noreply.github.com>
2023-04-27 08:02:34 -0400

    Add files via upload
    finish

---
+++ TCA-3-Magic-8-Ball-ChristianA7/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // 1. create a variable called magic8Ball and set it equal to an empty object
+const magic8Ball = {}
+  // 2. set the line of code below equal to an array of 5 different answers
+  magic8Ball.listOfAnswers = ["Yes", "No", "Very Likely", "Not yet", "Maybe"]
+
+  // 3. hide the answer element
+  document.getElementById("answer").style.display = "none";
+  magic8Ball.askQuestion = function(question) {
+    // 4. trigger shake animation
+  document.getElementsByClassName("ball")[0].classList.add("shake");
+  
+    // 5. change image to answer.png
+  document.getElementById("8ball").src = "img/answer.png";
+    // 6. fade in the answer
+    
+    var randomNumber = Math.random();
+    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+    var randomIndex = Math.floor(randomNumberForListOfAnswers);
+    var answer = this.listOfAnswers[randomIndex];
+    
+    document.getElementById("answer").innerHTML = answer;
+    console.log(answer);
+    document.getElementById("answer").classList.add("fade-in-text");
+    document.getElementById("answer").style.display = "inline";
+    
+    // 7. set the answer element's text to the answer variable
+    
+  };
+
+  var onClick = function() {
+
+    // 8. hide the answer element
+  document.getElementById("answer").style.display = "none";
+    // 9. change the image back to question.png
+document.getElementById("8ball").src = "img/question.png";
+
+    setTimeout(function(){
+      var question = prompt("ASK A YES/NO QUESTION!");
+      magic8Ball.askQuestion(question);
+    }, 500);
+
+
+  };
+
+  // 10. add a click handler on the question button that calls the function onClick
+  document.getElementById("questionButton").addEventListener("click", onClick);
+});
